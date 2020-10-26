7a527e835d070938b0420cd5d6dff31f1550815a
Pradeep Beniwal <47877972+CodeScripter@users.noreply.github.com>
2020-10-26 12:36:54 +0530

    Add files via upload

---
+++ MavenDemoSpring/src/main/java/com/arit/MavenDemoSpring/Inteli7.java
@@ -0,0 +1,13 @@
+package com.arit.MavenDemoSpring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Inteli7 implements Processor {
+
+	public void process()
+	{
+		System.out.println("Intel-i7 inside");
+	}
+}
